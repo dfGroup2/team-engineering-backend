3365219dc5a9de33ddee306de6cc7694e6c8d7a7
tangjm <jaredtang@hotmail.co.uk>
2021-12-15 10:29:52 +0000

    Added testing packages; Setup NodeServer and environment variables

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 const cors = require('cors');
+const express = require('express');
 
 
-function server() {
+const port = process.env.PORT;
+const host = process.env.HOST;
 
-}
+const app = express();
+
+const server = app.listen(port, host, () => {
+	const serverHOST = server.address().address;
+	const serverPORT = server.address().port;
+	console.log(`NodeServer listening at http://${serverHOST}:${serverPORT}/`);
+})
+
+
