23cd79815b28f069ec95e63c1658705a2c8fec7f
Anthony Sarsfield <sarsfielda@outlook.com>
2021-12-15 16:54:34 +0000

    created a test and setup graduateProfile testing area; had problems loading the json data

--- test/graduateProfile.test.js
+++ test/graduateProfile.test.js
@@ -2,10 +2,14 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const server = require('../server');
 const { expect } = require('chai');
+import testGraduateProfile from './testData/testGraduateProfile.json';
+import { GraduateProfile } from '../models/graduateProfile.model.js';
 
 chai.use(chaiHTTP);
 
-import GraduateProfile from '../models/graduateProfile.model'
+const path = '/graduateProfile';
+const id = '';
+
 describe('test for graduate profile route', () => {
     beforeEach(async () => {
         await GraduateProfile.deleteMany()
@@ -15,11 +19,21 @@ describe('test for graduate profile route', () => {
                 throw new Error();
             })
 
-        await GraduateProfile.insertMany()
+        await GraduateProfile.insertMany(testGraduateProfile)
             .then(() => console.log('entries added to database'))
             .catch(error => {
                 console.log(error)
                 throw new Error();
             })
-    })
-})
+    });
+
+    it('get request to /graduate profile rote', () => {
+        const response = await chai.request(server)
+            .get(`${path}/${id}`)
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.an.equal('Object');
+        expect(response.body).to.be.an.instanceOf(GraduateProfile);
+    });
+
+
+});
