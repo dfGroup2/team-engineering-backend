4ca2615f9df19ea0967d3d0513ad26b13553ab1a
Anthony Sarsfield <sarsfielda@outlook.com>
2021-12-15 11:22:43 +0000

    created graduate Profile schema

---
+++ models/graduateProfile.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+import { emailRegex } from '../js/regularExpressions';
+
+const { Schema } = mongoose;
+const graduateProfileSchema = new Schema({
+    firstName = { type: String, required: true },
+    lastName = { type: String, required: true },
+    personalEmail = { type: String, required: true, match: [emailRegex, 'Invalid email'] },
+    DFEmail = { type: String, required: true, match: [emailRegex, 'Invalid email'] },
+    github = { type: String },
+    linkedIn = { type: String },
+    phoneNumber = { type: Number },
+    profilePicture = { type: String },
+    personalStory = { type: [String] },
+    gender = { type: String, required: true },
+    nationality = { type: String, required: true },
+    personality = { type: String, required: true },
+});
+
+const GraduateProfile = mongoose.model('GraduateProfile', graduateProfileSchema);
+module.exports = graduateProfileSchema;
