241ff9018016d89ca543accb6b821fbcff6ac7d2
Anthony Sarsfield <sarsfielda@outlook.com>
2021-12-15 10:49:58 +0000

    set up mongoose connection to mongoDB

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
 
 
 const port = process.env.PORT;
 const host = process.env.HOST;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const main = async () => {
+    console.log(`Connecting to database at ${process.env.DB_URI}`);
+    await mongoose.connect(process.env.DB_URI);
+};
+main().catch(error => console.log(error));
+
+
 
 const server = app.listen(port, host, () => {
-	const serverHOST = server.address().address;
-	const serverPORT = server.address().port;
-	console.log(`NodeServer listening at http://${serverHOST}:${serverPORT}/`);
+    const serverHOST = server.address().address;
+    const serverPORT = server.address().port;
+    console.log(`NodeServer listening at http://${serverHOST}:${serverPORT}/`);
+
 })
 
 
