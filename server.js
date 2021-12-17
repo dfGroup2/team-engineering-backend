ad6735f4eb431c6b8e897621b37905bf01597263
tangjm <jaredtang@hotmail.co.uk>
2021-12-17 11:55:52 +0000

    Added function to populate the roles collection if collection is empty

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 const cors = require('cors');
+const Role = require('./models/authentication/role.model');
 
+function initial() {
+	Role.estimatedDocumentCount((err, count) => {
+		if (!err && count === 0) {
+			new Role({
+				name: "graduateUser"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
 
-function server() {
+				console.log("added 'graduateUser' to roles collection");
+			});
 
+			new Role({
+				name: "industryPartner"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
+
+				console.log("added 'industryPartner' to roles collection");
+			});
+
+			new Role({
+				name: "dfAdmin"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
+
+				console.log("added 'dfAdmin' to roles collection");
+			});
+		}
+	});
 }
