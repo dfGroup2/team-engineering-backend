0d92df6393a902e40b5878731edb774f82a25ff3
tangjm <jaredtang@hotmail.co.uk>
2021-12-17 11:55:52 +0000

    Added function to populate the roles collection if collection is empty

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 const cors = require('cors');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const graduateProfileRouter = require('./routes/graduateProfile');
@@ -11,23 +12,61 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Role = require('./models/authentication/role.model');
+
+function initial() {
+	Role.estimatedDocumentCount((err, count) => {
+		if (!err && count === 0) {
+			new Role({
+				name: "graduateUser"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
+
+				console.log("added 'graduateUser' to roles collection");
+			});
+
+			new Role({
+				name: "industryPartner"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
+
+				console.log("added 'industryPartner' to roles collection");
+			});
+
+			new Role({
+				name: "dfAdmin"
+			}).save(err => {
+				if (err) {
+					console.log("error", err);
+				}
+
+				console.log("added 'dfAdmin' to roles collection");
+			});
+		}
+	});
+}
 // routes
 app.use(`/graduateProfiles`, graduateProfileRouter);
 
 
 const main = async () => {
-    console.log(`Connecting to database at ${process.env.DB_URI}`);
-    await mongoose.connect(process.env.DB_URI);
+	console.log(`Connecting to database at ${process.env.DB_URI}`);
+	await mongoose.connect(process.env.DB_URI);
 };
 main().catch(error => console.log(error));
 
 const server = app.listen(port, host, () => {
-    const serverHOST = server.address().address;
-    const serverPORT = server.address().port;
-    console.log(`NodeServer listening at http://${serverHOST}:${serverPORT}/`);
+	const serverHOST = server.address().address;
+	const serverPORT = server.address().port;
+	console.log(`NodeServer listening at http://${serverHOST}:${serverPORT}/`);
 
 })
 
 module.exports = server;
+
 
 
