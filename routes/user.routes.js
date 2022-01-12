cbbe4f4e1cf4738e2a9e6da6a8b8f7e5b3d0dcf9
KatieStocker <stocker_katie@aol.co.uk>
2022-01-12 15:39:41 +0000

    added a put route for the graduateUser data

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -31,7 +31,27 @@ router.get(`/graduateTraining/:id`, [authJWT.verifyToken, authJWT.isGraduateUser
 // id is the id of the logged in user
 router.get(`/graduateUsers/:id`, [authJWT.verifyToken, authJWT.isGraduateUser], graduateUserHandler);
 
+const { GraduateUser } = require('../models/graduateUser.model');
+router.route('/graduateUsers/:id')
+	.put((req, res) => {
+		const id = req.params.id;
+		GraduateUser.findById(id, (error, graduateUser) => {
+			if (!graduateUser) {
+				res.status(404).send('That user data cannot be found');
+			}
+			else {
+				graduateUser.graduateProfile = req.body.graduateProfile;
+				graduateUser.graduateTraining = req.body.graduateTraining;
+				graduateUser.personalInfo = req.body.personalInfo;
+				graduateUser.personalStory = req.body.personalStory;
 
-
+				graduateUser.save()
+					.then(graduateUser => {
+						res.status(201).json({ 'graduateUser': `${graduateUser} data added successfully` });
+					})
+					.catch(err => res.status(400).send({ 'error': `Adding to data failed: ${err}` }));
+			}
+		});
+	});
 
 module.exports = router;
